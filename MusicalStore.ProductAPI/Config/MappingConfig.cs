7bf5668c6f15e0a2ae36bfac87d15723a06246e6
Luiz Carlos dos Santos <luizbokan9@gmail.com>
2023-03-10 12:48:50 -0300

    iniciando projeto loja de instrumentos musicais

---
+++ MusicalStore.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MusicalStore.ProductAPI.Data.ValueObjects;
+using MusicalStore.ProductAPI.Model;
+
+namespace MusicalStore.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ MusicalStore.Web/Service/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using MusicalStore.Web.Models;
+
+namespace MusicalStore.Web.Service.IService
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
